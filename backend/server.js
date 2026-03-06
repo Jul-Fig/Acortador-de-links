aab1b587b1214d40f67c4b18986169e491585a26
Julian <figueredo.julian96@gmail.com>
2026-03-05 20:52:49 -0500

    correción proxy backend

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const urlRoutes = require('./routes/url.routes')
 const errorHandler = require('./middleware/error.middleware')
 
 const app = express()
+app.set('trust proxy', 1)
 app.use(helmet())
 app.use(cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:4200',
